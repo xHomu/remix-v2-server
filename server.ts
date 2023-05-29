b79a796063654b9e42184b3c8134c08ea5774af6
fred <nobisan@gmail.com>
2023-05-29 10:12:27 -0700

    better cache busting

--- server.ts
+++ server.ts
@@ -1,12 +1,17 @@
-import * as fs from "node:fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
 import chokidar from "chokidar";
 import express from "express";
 import compression from "compression";
 import morgan from "morgan";
 import { createRequestHandler } from "@remix-run/express";
-import { broadcastDevReady, installGlobals } from "@remix-run/node";
+import { ServerBuild, broadcastDevReady, installGlobals } from "@remix-run/node";
 
-import * as build from "./build/index.js";
+// @ts-ignore - this file may not exist if you haven't built yet, but it will
+// definitely exist by the time the dev or prod server actually runs.
+import * as remixBuild from "./build/index.js";
+const build = remixBuild as unknown as ServerBuild
+let devBuild = build
 
 const BUILD_PATH = "./build/index.js";
 
@@ -34,7 +39,16 @@ app.use(morgan("tiny"));
 app.all(
   "*",
   process.env.NODE_ENV === "development"
-    ? createDevRequestHandler()
+    ? async (req, res, next) => {
+      try {
+        return createRequestHandler({
+          build: devBuild,
+          mode: "development",
+        })(req, res, next);
+      } catch (error) {
+        next(error);
+      }
+    }
     : createRequestHandler({
         build,
         mode: process.env.NODE_ENV,
@@ -42,6 +56,7 @@ app.all(
 );
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, async () => {
   console.log(`Express server listening on port ${port}`);
 
@@ -50,32 +65,15 @@ app.listen(port, async () => {
   }
 });
 
-function createDevRequestHandler() {
-  // initial build
-  /**
-   * @type { import('@remix-run/node').ServerBuild | Promise<import('@remix-run/node').ServerBuild> }
-   */
-  let devBuild = build;
+// during dev, we'll keep the build module up to date with the changes
+if (process.env.NODE_ENV === 'development') {
+	async function reloadBuild() {
+		devBuild = await import(`${BUILD_PATH}?update=${Date.now()}`)
+		broadcastDevReady(devBuild)
+	}
 
-  const watcher = chokidar.watch(BUILD_PATH, { ignoreInitial: true });
-
-  watcher.on("all", async () => {
-    // 1. purge require cache && load updated server build
-    const stat = fs.statSync(BUILD_PATH);
-    devBuild = import(BUILD_PATH + "?t=" + stat.mtimeMs);
-    // 2. tell dev server that this app server is now ready
-    broadcastDevReady(await devBuild);
-  });
-
-  return async (req, res, next) => {
-    try {
-      //
-      return createRequestHandler({
-        build: await devBuild,
-        mode: "development",
-      })(req, res, next);
-    } catch (error) {
-      next(error);
-    }
-  };
+	const dirname = path.dirname(fileURLToPath(import.meta.url))
+	const watchPath = path.join(dirname, BUILD_PATH).replace(/\\/g, '/')
+	const watcher = chokidar.watch(watchPath, { ignoreInitial: true })
+	watcher.on('all', reloadBuild)
 }
