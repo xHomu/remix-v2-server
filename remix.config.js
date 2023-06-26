7b760834e3055ea83ba9844478918f010787ef03
xHomu <84349818+xHomu@users.noreply.github.com>
2023-06-26 11:51:09 -0700

    Add a v1 branch for comparison

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
+module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  serverModuleFormat: "esm",
-  tailwind: true,
-  postcss: true,
+  serverModuleFormat: "cjs",
   future: {
-    v2_dev: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
