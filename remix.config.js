e98296334cab555a82a36af0195bd4e2badf4ac8
xHomu <84349818+xHomu@users.noreply.github.com>
2023-06-26 11:34:33 -0700

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,15 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
+module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  serverModuleFormat: "esm",
+  serverModuleFormat: "cjs",
   tailwind: true,
   postcss: true,
   future: {
-    unstable_dev: true,
+    v2_dev: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
