5b33f0cf8af2310d8017b92e8277f810bce36370
xHomu <84349818+xHomu@users.noreply.github.com>
2023-06-26 12:16:12 -0700

    try to make github happy

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+  preflight: false,
+  }
+} satisfies Config
+
