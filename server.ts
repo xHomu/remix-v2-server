6c2f3f35f64138ab0a72546f6226b1673c0a1c6d
xHomu <84349818+xHomu@users.noreply.github.com>
2023-07-07 15:55:50 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -29,19 +29,20 @@ app.use(express.static("public", { maxAge: "1h" }));
 
 app.use(morgan("tiny"));
 
+// Check if the server is running in development mode and use the devBuild to reflect realtime changes in the codebase.
 app.all(
   "*",
   process.env.NODE_ENV === "development"
     ? async (req, res, next) => {
-      try {
-        return createRequestHandler({
-          build: require(BUILD_DIR),
-          mode: process.env.NODE_ENV,
-        })(req, res, next);
-      } catch (error) {
-        next(error);
+        try {
+          return createRequestHandler({
+            build: require(BUILD_DIR),
+            mode: process.env.NODE_ENV,
+          })(req, res, next);
+        } catch (error) {
+          next(error);
+        }
       }
-    }
     : createRequestHandler({
         build: require(BUILD_DIR),
         mode: process.env.NODE_ENV,
@@ -59,16 +60,22 @@ app.listen(port, async () => {
 });
 
 // during dev, we'll keep the build module up to date with the changes
-if (process.env.NODE_ENV === 'development') {
-	const watcher = chokidar.watch(BUILD_DIR, {
-		ignored: ['**/**.map'],
-	})
-	watcher.on('all', () => {
-		for (const key in require.cache) {
-			if (key.startsWith(BUILD_DIR)) {
-				delete require.cache[key]
-			}
-		}
-		broadcastDevReady(require(BUILD_DIR))
-	})
+async function updateServer() {
+  // 1. purge require cache && load updated server build
+  for (const key in require.cache) {
+    if (key.startsWith(BUILD_DIR)) {
+      delete require.cache[key];
+    }
+  }
+  // 2. tell dev server that this app server is now ready
+  broadcastDevReady(require(BUILD_DIR));
+}
+
+if (process.env.NODE_ENV === "development") {
+  const watcher = chokidar.watch(BUILD_DIR, {
+    ignored: ["**/**.map"],
+  });
+
+  watcher.on("add", updateServer);
+  watcher.on("change", updateServer);
 }
