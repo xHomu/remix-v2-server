bc57179cf82b50de4218f45482c1d69d07d44439
xHomu <84349818+xHomu@users.noreply.github.com>
2023-07-13 15:35:21 -0700

    revising based on https://remix.run/docs/en/dev/guides/manual-mode#1b-esm-import-cache-busting

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
+      <ul className="list-disc">
         <li>
           <a
             target="_blank"
