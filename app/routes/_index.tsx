c375c7279f872f322755972a67b81e553e5d1e6d
xHomu <84349818+xHomu@users.noreply.github.com>
2023-07-19 11:14:09 -0700

    reset to v1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,8 +11,8 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li className="list-disc">
+      <ul className="list-disc">
+        <li>
           <a
             target="_blank"
             href="https://remix.run/tutorials/blog"
--- postcss.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "postcss-load-config";
-
-export default {
-  plugins: {
-    autoprefixer: {},
-  },
-} satisfies Config;
