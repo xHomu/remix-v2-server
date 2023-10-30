9acbbf65b33fbc0c424b588f8acf51c39b83e053
xHomu <84349818+xHomu@users.noreply.github.com>
2023-10-30 12:14:38 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -63,7 +63,7 @@ app.listen(port, async () => {
 
   // send "ready" message to dev server
   if (process.env.NODE_ENV === "development") {
-    broadcastDevReady(build);
+    await broadcastDevReady(build);
   }
 });
 
@@ -80,7 +80,7 @@ function createDevRequestHandler(): RequestHandler {
     }
 
     // 2. tell dev server that this app server is now up-to-date and ready
-    broadcastDevReady(build);
+    await broadcastDevReady(build);
   }
 
   chokidar
